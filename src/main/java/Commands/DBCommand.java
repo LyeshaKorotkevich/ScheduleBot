655e21a9d226532b7cdcf7c3f9929a3b7d9636d7
leha <torlesh18112003@gmail.com>
2022-05-11 22:54:18 +0300

    добавил стикеры но стало работать через раз

--- src/main/java/Commands/DBCommand.java
+++ src/main/java/Commands/DBCommand.java
@@ -1,5 +1,7 @@
 package Commands;
 
+import com.vdurmont.emoji.EmojiParser;
+
 import java.sql.*;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -9,7 +11,7 @@ public class DBCommand {
     private static final String DB_URL = "jdbc:postgresql://194.195.241.62:5432/l_korotkevich_db";
     private static final String USER = "l_korotkevich";
     private static final String PASS = "i-opqdlr";
-
+    private static final String[] emojis = {"1️⃣", "2️⃣", "3️⃣", "4️⃣"};
 
     public static String getDay(String day) {
         try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASS)) {
@@ -20,11 +22,11 @@ public class DBCommand {
             ResultSet rs=ps.executeQuery();
             StringBuilder text = new StringBuilder();
             while (rs.next()){
-                text.append(rs.getString(3)).append(" ");
+                text.append(emojis[rs.getInt(6)-1]).append(rs.getString(3)).append(" ");
                 text.append(rs.getString(4)).append(" ");
                 text.append(rs.getString(5)).append("\n");
             }
-            return text.toString();
+            return EmojiParser.parseToAliases(text.toString());
         } catch (SQLException e) {
             System.out.println("Connection Failed");
             e.printStackTrace();
@@ -41,12 +43,12 @@ public class DBCommand {
             HashMap<String, String> days= new LinkedHashMap<>();
             while (rs.next()){
                 if (days.containsKey(WEEK_DAY.valueOf(rs.getString(2)).getDescription())) {
-                    String text = rs.getString(3) + " " +
+                    String text = emojis[rs.getInt(6)-1] + rs.getString(3) + " " +
                             rs.getString(4) + " " +
                             rs.getString(5) + "\n";
                     days.put(WEEK_DAY.valueOf(rs.getString(2)).getDescription(), days.get(WEEK_DAY.valueOf(rs.getString(2)).getDescription()) + text);
                 }else{
-                    String text = rs.getString(3) + " " +
+                    String text = emojis[rs.getInt(6)-1] + rs.getString(3) + " " +
                             rs.getString(4) + " " +
                             rs.getString(5) + "\n";
                     days.put(WEEK_DAY.valueOf(rs.getString(2)).getDescription(), text);
